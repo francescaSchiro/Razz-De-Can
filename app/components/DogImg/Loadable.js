3949070e535727e8b5c288c9a0636fde71702b1d
Schiro Francesca <francesca.schiro@sisal.it>
2019-02-13 11:28:51 +0100

    ADD react-loadable for DogImg

--- app/components/DogImg/Loadable.js
+++ app/components/DogImg/Loadable.js
@@ -1,9 +1,33 @@
 /**
- * Asynchronously loads the component for Breeds
+ * Asynchronously loads the component for DogImg
  */
 import React from 'react';
-import loadable from 'loadable-components';
+// import loadable from 'loadable-components';
+import Loadable from 'react-loadable';
+// import Loading from './my-loading-component';
 
-export default loadable(() => import('./index'), {
-  fallback: <div style={{ color: 'black', fontSize: '5rem' }}>Loading...</div>,
+function Loading() {
+  return (
+    <div
+      style={{
+        zIndex: 100,
+        // backgroundColor: 'black',
+        color: 'black',
+        height: '500px',
+        width: '500px',
+      }}
+    >
+      <h1>Loading...</h1>
+    </div>
+  );
+}
+const LoadableComponent = Loadable({
+  loader: () => import('./index'),
+  loading: Loading,
 });
+
+export default LoadableComponent;
+
+// export default loadable(() => import('./index'), {
+//   fallback: <div style={{ color: 'black', fontSize: '5rem' }}>Loading...</div>,
+// });
